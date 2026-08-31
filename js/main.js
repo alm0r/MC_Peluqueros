afdbce14b41da6c5be37203c320f294defd7ed2d
alm0r <almor.dev@protonmail.com>
2026-09-01 00:16:47 +0200

    html format 1.0

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 console.log("main script working");
+
+// document.querySelector("body").setAttribute("style", "background-color: blue");
+
+// document.querySelector("body").style.background = "grey";
