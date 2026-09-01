553f05915ac38f4d7ed83f9c0ed3700aa6c2e7a5
alm0r <almor.dev@protonmail.com>
2026-09-01 13:56:22 +0200

    finish header layout

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ console.log("main script working");
 // document.querySelector("body").setAttribute("style", "background-color: blue");
 
 // document.querySelector("body").style.background = "grey";
+
+// const navLinks = document.querySelectorAll(".main-nav-link");
+// console.log("test");
+
+// navLinks.forEach((e) => {
+//   e.addEventListener("click", function () {
+//     console.log("click!");
+//     e.style.color = "{var(--textColor)";
+//   });
+// });
